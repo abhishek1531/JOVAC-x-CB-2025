13c5677be7febd221233d8733bfccb995ffd3075
Abhishek <abhi.singh15dec@gmail.com>
2025-06-25 23:59:29 +0530

    Add files via upload

---
+++ ifelse.js
@@ -0,0 +1,98 @@
+// Check if a number is even or odd
+
+// let num = 7;
+// if(num % 2 == 0 ){
+//     console.log("Even");
+// }
+// else{
+//     console.log("Odd");
+// }
+
+
+
+// Check if a person is eligible to vote
+
+// let age = 16;
+// if (age >= 18) {
+//   console.log("Eligible to vote");
+// } 
+// else {
+//   console.log("Not eligible to vote");
+// }
+
+
+
+// Find the largest of two numbers 
+
+// let a = 15;
+// let b = 20;
+// if (a > b) {
+//   console.log(a + " is greater");
+// } 
+// else {
+//   console.log(b + " is greater");
+// }
+
+
+
+
+// Find the largest of three numbers
+
+// let x = 20;
+// let y = 40;
+// let z = 30;
+
+// if (x > y && x > z) {
+//   console.log(x + " is the largest");
+// } 
+// else if (y > z) {
+//   console.log(y + " is the largest");
+// }
+//  else {
+//   console.log(z + " is the largest");
+// }
+
+
+
+
+// Check if a year is a leap year
+
+// let year = 2024;
+
+// if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+//   console.log("Leap year");
+// } 
+// else {
+//   console.log("Not a leap year");
+// }
+
+
+
+
+// Grade system based on marks
+
+// let marks = 76;
+
+// if (marks >= 90) {
+//   console.log("Grade: A");
+// } else if (marks >= 75) {
+//   console.log("Grade: B");
+// } else if (marks >= 60) {
+//   console.log("Grade: C");
+// } else if (marks >= 40) {
+//   console.log("Grade: D");
+// } else {
+//   console.log("Fail");
+// }
+
+
+// Check if character is vowel or consonant
+
+// let ch = 'e';
+
+// if (ch === 'a' || ch === 'e' || ch === 'i' || ch === 'o' || ch === 'u') {
+//   console.log("Vowel");
+// } 
+// else {
+//   console.log("Consonant");
+// }
